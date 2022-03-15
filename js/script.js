f5ab3c54b49126bb085fa4a230ee92077ffd1541
ericthmoritsuka <ericthmoritsuka@gmail.com>
2022-03-15 10:11:11 -0300

    perguntas em seguros drop down elements js, css e html

--- js/script.js
+++ js/script.js
@@ -22,3 +22,21 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarHandler(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarHandler);
+}
+perguntas.forEach(eventosPerguntas);
